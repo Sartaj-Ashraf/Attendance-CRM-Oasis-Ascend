882fd8932d1684f3e22e23d19a978d8199166d48
Umaid Hamid <umaid.oasis@gmail.com>
2025-12-22 12:29:33 +0530

    12

--- backend/Database/Database.js
+++ backend/Database/Database.js
@@ -3,7 +3,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log("database connected");
-    // hi 
+    
   } catch (e) {
     console.log("error in connect in database", e.message);
     process.exit(1);

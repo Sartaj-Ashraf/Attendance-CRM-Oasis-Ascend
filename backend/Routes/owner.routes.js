2db65bbf8b85a7af40302a1188738298c633fa66
Umaid Hamid <umaid.oasis@gmail.com>
2025-12-31 15:10:53 +0530

    routes name changes

--- backend/Routes/owner.routes.js
+++ backend/Routes/owner.routes.js
@@ -12,15 +12,16 @@ import {
   GetManagers,
   BlockedUsers,
   replaceManager,
-  getAllEmployees ,
-  createOwner
+  // getAllEmployees,
+  getAllEmployeesForAttendance,
+  createOwner,
 } from "../Controllers/owner.controller.js";
 import { authMiddleware, isAdmin } from "../middleware/auth.middleware.js";
 import {
   markAttendance,
   markBulkAttendance,
 } from "../Controllers/attendence.controller.js";
-router.post("/create-owner",createOwner)
+router.post("/create-owner", createOwner);
 router.post("/create", authMiddleware, isAdmin, createUser); // creating user
 router.post("/edituser/:id", authMiddleware, isAdmin, editUser); // edit user
 router.patch("/disableaccount/:id", authMiddleware, isAdmin, disableaccount); // block  user
@@ -30,11 +31,16 @@ router.post("/markattendence", authMiddleware, isAdmin, markAttendance);
 router.post("/attendance/bulk", authMiddleware, markBulkAttendance);
 router.post("/deleteUser/:id", authMiddleware, isAdmin, deleteUser);
 router.get("/getAllUsers", authMiddleware, isAdmin, getAllDepartmentUser);
-router.get("/getAllEmployee", authMiddleware, isAdmin, GetAllEmployee);
 router.post("/getBlockedUser", authMiddleware, isAdmin, getBlockedUser);
 router.get("/getManagers", authMiddleware, isAdmin, GetManagers);
 router.get("/getBlockedUsers", authMiddleware, isAdmin, BlockedUsers);
-router.get("/getAllEmployees", authMiddleware, isAdmin, getAllEmployees );
 
+router.get("/getAllEmployee", authMiddleware, isAdmin, GetAllEmployee);
+router.get(
+  "/getAllEmployeesForAttendance",
+  authMiddleware,
+  isAdmin,
+  getAllEmployeesForAttendance
+);
 
 export default router;
